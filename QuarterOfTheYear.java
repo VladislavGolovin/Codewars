dcbc15f579ad566ea6e815910ab1454c0a20f4ae
Vladislav Golovin <golovin.vladislav@gmail.com>
2023-02-22 16:04:00 +0300

    Added solution for QuaterOfTheYear kata

--- QuarterOfTheYear.java
+++ QuarterOfTheYear.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
+
 /*
+    Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
-Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+    For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
 
-For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+    Constraint:
 
-Constraint:
-
-    1 <= month <= 12
-
-*/
+        1 <= month <= 12
+ */
 public class QuarterOfTheYear {
+    
     public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input month: ");
+        int month = scan.nextInt();
+
+        scan.close();
+        System.out.println(quarterOf(month));
     }
+
+    public static int quarterOf(int month) {
+        return month > 9 ? 4 : month > 6 ? 3 : month > 3 ? 2 : 1;
+      }
+      
 }
